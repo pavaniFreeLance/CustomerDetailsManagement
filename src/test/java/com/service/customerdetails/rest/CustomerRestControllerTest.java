7139fe2241584e926ed74ef2a97105f0b6e6d9f1
Pavani Bandi <pavani.bandi@vanderlande.com>
2020-08-24 22:03:04 +0200

    Enabling Junit by adding dependency

--- src/test/java/com/service/customerdetails/rest/CustomerRestControllerTest.java
+++ src/test/java/com/service/customerdetails/rest/CustomerRestControllerTest.java
@@ -1,6 +1,6 @@
 package com.service.customerdetails.rest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
